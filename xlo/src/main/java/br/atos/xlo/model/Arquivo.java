6ae36b8b3862b8e48ef2ae2887fffe8544184b21
Thiago Santana <thiago.2.santana@atos.net>
2022-12-01 21:22:33 -0300

    fix: estruras de entidades relacionadas

--- xlo/src/main/java/br/atos/xlo/model/Arquivo.java
+++ xlo/src/main/java/br/atos/xlo/model/Arquivo.java
@@ -1,37 +1,42 @@
 package br.atos.xlo.model;
+
 import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import org.springframework.data.annotation.Id;
 
-@Entity(name = "TB_ARQUIVO")
-public class Arquivo implements Serializable{
-	
+@Entity
+@Table(name = "TB_ARQUIVO")
+public class Arquivo implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "NI_COD_ARQUIVO")
 	private int codArquivo;
 	@Column(name = "VC_END_ARQUIVO")
 	private String endArquivo;
-	
+
 	public int getCodArquivo() {
 		return codArquivo;
 	}
+
 	public void setCodArquivo(int codArquivo) {
 		this.codArquivo = codArquivo;
 	}
+
 	public String getEndArquivo() {
 		return endArquivo;
 	}
+
 	public void setEndArquivo(String endArquivo) {
 		this.endArquivo = endArquivo;
 	}
-
 
 }
--- xlo/src/main/java/br/atos/xlo/model/Usuario.java
+++ xlo/src/main/java/br/atos/xlo/model/Usuario.java
@@ -1,35 +1,35 @@
 package br.atos.xlo.model;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-import org.springframework.data.annotation.Id;
+@Entity
+@Table(name = "TB_USUARIO")
+public class Usuario implements Serializable {
 
-@Entity(name = "TB_USUARIO")
-public class Usuario implements Serializable{
-	
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "NI_COD_USUARIO")
 	private int codUsuario;
-	
-	@ManyToMany(mappedBy="usuarios")
+
+	@ManyToMany()
 	private List<Role> roles;
-	
-	@ManyToOne
+
+	@OneToMany()
 	private List<Endereco> enderecos;
-	
+
 	@Column(name = "VC_NOME")
 	private String nome;
 	@Column(name = "VC_CPF")
@@ -44,55 +44,85 @@ public class Usuario implements Serializable{
 	private Date dtCadastro;
 	@Column(name = "DTH_ATUALIZACAO")
 	private Date dtAtualizacao;
-	
+
 	public int getCodUsuario() {
 		return codUsuario;
 	}
+
 	public void setCodUsuario(int codUsuario) {
 		this.codUsuario = codUsuario;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public int getStatusUsuario() {
 		return statusUsuario;
 	}
+
 	public void setStatusUsuario(int statusUsuario) {
 		this.statusUsuario = statusUsuario;
 	}
+
 	public Date getDtCadastro() {
 		return dtCadastro;
 	}
+
 	public void setDtCadastro(Date dtCadastro) {
 		this.dtCadastro = dtCadastro;
 	}
+
 	public Date getDtAtualizacao() {
 		return dtAtualizacao;
 	}
+
 	public void setDtAtualizacao(Date dtAtualizacao) {
 		this.dtAtualizacao = dtAtualizacao;
 	}
 
-	
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
 }
