3b08b9a41956ef91fdf2554acbb1f6b396fae251
aldopassossp <aldopassos@gmail.com>
2022-12-07 20:42:51 -0300

    style: adicionadas informações da API no Swagger

--- xlo/src/main/java/br/atos/xlo/config/SwaggerConfig.java
+++ xlo/src/main/java/br/atos/xlo/config/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package br.atos.xlo.config;
 
+import javax.swing.Spring;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,6 +21,19 @@ public class SwaggerConfig {
           .select()
           .apis(RequestHandlerSelectors.any())
           .paths(PathSelectors.any())
-          .build();
+          .build()
+          .useDefaultResponseMessages(false)
+          .apiInfo(apiInfo());
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API XLO - Motors")
+                .description("Sistema de venda de automotores")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("XLO - Motors", "https://xlomotors.com.br", "contato@xlomotors.com.br"))
+                .build();
     }
 }
