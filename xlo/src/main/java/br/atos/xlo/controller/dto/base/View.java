cc6568dcc31c3a615e2ed0c166d767ec491aecc2
Thiago Santana <thiago.2.santana@atos.net>
2022-12-07 21:13:23 -0300

    feat: mapeamento de dto para swagger

---
+++ xlo/src/main/java/br/atos/xlo/controller/dto/base/View.java
@@ -0,0 +1,18 @@
+package br.atos.xlo.controller.dto.base;
+
+public interface View {
+
+	public static interface ControllerView {
+		public static interface POST {
+
+		}
+
+		public static interface PUT {
+
+		}
+
+		public static interface GET {
+
+		}
+	}
+}
