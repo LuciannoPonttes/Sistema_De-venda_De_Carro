ab02978f45eb1e319416d6b904e7d636eb171ee8
Thiago Santana <thiago.2.santana@atos.net>
2022-12-12 21:45:34 -0300

    feat: services para login e senha criptografada

--- xlo/src/main/java/br/atos/xlo/model/Login.java
+++ xlo/src/main/java/br/atos/xlo/model/Login.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -16,12 +18,24 @@ public class Login implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "NI_COD_LOGIN")
+	private int codLogin;
+
 	@OneToOne
 	@JoinColumn(name = "VC_USUARIO", referencedColumnName = "VC_EMAIL")
 	private Usuario usuario;
 
 	@Column(name = "VC_SENHA")
 	private String senha;
+
+	public int getCodLogin() {
+		return codLogin;
+	}
+
+	public void setCodLogin(int codLogin) {
+		this.codLogin = codLogin;
+	}
 
 	public Usuario getUsuario() {
 		return usuario;
---
+++ xlo/src/main/java/br/atos/xlo/services/LoginServiceImpl.java
@@ -0,0 +1,41 @@
+package br.atos.xlo.services;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.atos.xlo.dto.LoginDTO;
+import br.atos.xlo.dto.UsuarioDTO;
+import br.atos.xlo.model.Login;
+import br.atos.xlo.model.Usuario;
+import br.atos.xlo.repository.LoginRepository;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+	@Autowired
+	LoginRepository loginRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	ModelMapper modelMapper;
+
+	public LoginDTO adicionar(LoginDTO loginDTO, UsuarioDTO usuarioDTO) {
+		Login login = new Login();
+		login.setUsuario(modelMapper.map(usuarioDTO, Usuario.class));
+		login.setSenha(this.generatePassword(loginDTO.getSenha()));
+
+		return modelMapper.map(loginRepository.save(login), LoginDTO.class);
+	}
+
+	public String generatePassword(String rawPassword) {
+		return passwordEncoder.encode(rawPassword);
+	}
+
+	public boolean verifyPassword(String rawPassword, String encodedPassword) {
+		return passwordEncoder.matches(rawPassword, encodedPassword);
+	}
+}
